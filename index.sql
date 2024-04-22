46c47c4eb6309d7f0e556062049e79827d83fd6e
Sahoon Hussain Ansari <162339117+Sahoon1@users.noreply.github.com>
2024-04-22 20:32:04 +0500

    work

---
+++ index.sql
@@ -0,0 +1,33 @@
+CREAT NEW DATABASE NAME;
+
+DROP TABLE NAME;
+
+CREAT TABLE NAME(
+ ID INT,
+ FULLNAME VARCHAR(30);
+PHONE INT
+);
+
+DROP TABLE NAME;
+
+ALTER TABLE NAME
+ADD column_name;
+
+ALTER TABLE NAME
+DROP column_name;
+
+
+INSERT INTO table_name (column1, column2, column3, ...)
+VALUES (value1, value2, value3, ...);
+
+SELECT * FROM TABLENAME;
+
+SELECT * FROM TABLENAME FROM WHERE COLUMN=1;
+
+DELETE * FROM TABLENAME FROM WHERE COLUMN=1;
+
+UPDATE table_name
+SET column1 = value1, column2 = value2, ...
+WHERE COLUMN=1;
+
+
